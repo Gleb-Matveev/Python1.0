fc2e8306ed17849610317de2923db76aa772129b
Gleb-Matveev <61318934+Gleb-Matveev@users.noreply.github.com>
2023-01-19 14:52:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-def print_hi(name):
-    num1 = int(input("Enter a first num: "))
-    print(num1)
- 
- 
+import sys
+
+def calc(nm1, nm2, op):
+    if op == "+":
+        res = nm1 + nm2
+        return res
+    elif op == "-":
+        res = nm1 - nm2
+        return res
+    elif op == "@":
+        res = nm1 * nm2
+        return res
+    elif op == "/":
+        if nm2 == 0:
+            res = "undefined"
+            return res
+        else:
+            res = nm1 / nm2
+            return res
+
+
+def print_hi(num1, num2, opera):
+    print(calc(num1, num2, opera))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
+
