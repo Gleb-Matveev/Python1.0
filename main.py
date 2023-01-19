abd20cab67e538713fd94a4b0dc7b38e5958cdeb
Gleb-Matveev <61318934+Gleb-Matveev@users.noreply.github.com>
2023-01-19 13:12:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,11 @@ def calc(nm1, nm2, op):
         else:
             res = nm1 / nm2
             return res
+        
+
+ def print_hi(name):
+     print(f'Hi, {name}')
 
 
-def main():
-    opera = input("Enter an operation: ")
-    num1 = int(input("Enter a first num: "))
-    num2 = int(input("Enter a second num: "))
-    print(calc(num1, num2, opera))
-
-
-if __name__ == '__main__':
-    main()
+ if __name__ == '__main__':
+     print_hi('PyCharm')
