775748f8698f177e4fc07dcb7e02091528506343
Gleb-Matveev <61318934+Gleb-Matveev@users.noreply.github.com>
2023-01-19 13:10:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 
-def print_hi(name):
-    print(f'Hi, {name}')
+def calc(nm1, nm2, op):
+    if op == "+":
+        res = nm1 + nm2
+        return res
+    elif op == "-":
+        res = nm1 - nm2
+        return res
+    elif op == "*":
+        res = nm1 * nm2
+        return res
+    elif op == "/":
+        if nm2 == 0:
+            res = "undefined"
+            return res
+        else:
+            res = nm1 / nm2
+            return res
+
+
+def main():
+    opera = input("Enter an operation: ")
+    num1 = int(input("Enter a first num: "))
+    num2 = int(input("Enter a second num: "))
+    print(calc(num1, num2, opera))
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
