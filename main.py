ba84128252e443a158d3e4f31493554fa73ed8bc
Gleb-Matveev <61318934+Gleb-Matveev@users.noreply.github.com>
2023-01-19 14:22:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-def calc(nm1, nm2, op):
-    if op == "+":
-        res = nm1 + nm2
-        return res
-    elif op == "-":
-        res = nm1 - nm2
-        return res
-    elif op == "*":
-        res = nm1 * nm2
-        return res
-    elif op == "/":
-        if nm2 == 0:
-            res = "undefined"
-            return res
-        else:
-            res = nm1 / nm2
-            return res
- 
- 
 def print_hi(name):
     print(f'Hi, {name}')
  
